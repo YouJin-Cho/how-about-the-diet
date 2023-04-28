02db2162e34c3e170a63d1185099de29f0e4bd6a
YouJin-Cho <hycoho1996@gmail.com>
2023-04-28 21:10:42 +0900

    Nutrients update

--- src/Components/MainContainer.tsx
+++ src/Components/MainContainer.tsx
@@ -3,6 +3,7 @@ import Login from "./Login"
 import Registration from "./Registration"
 import MainPage from "./MainPage"
 import DetailFood from "./DetailFood"
+import Nutrients from "./Nutrients"
 
 
 const MainContainer = () => {
@@ -11,6 +12,7 @@ const MainContainer = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/registration" element={<Registration />} />
       <Route path="/" element={<MainPage />} />
+      <Route path="/nutrients" element={<Nutrients />} />
       <Route path="/detail" element={<DetailFood />} />
     </Routes>
   )
